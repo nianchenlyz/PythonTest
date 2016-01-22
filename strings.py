bea08f7665e13866bef132a05dbbcb1cd006ee1d
Kamil Wozniak <kamil.m.wozniak@gmail.com>
2016-01-22 13:04:21 +0000

    Shorter way of trimming string

--- strings.py
+++ strings.py
@@ -2,6 +2,12 @@
 
 # Truncating long strings
 
-long_string = "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
-string_after_trimming = long_string[:97] + '...' if len(long_string) > 97 else long_string 
+long_string = "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstring"
+string_after_trimming = long_string[:97] + '...' if len(long_string) > 97 else long_string
 # result 'loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo...'
+
+# shorter way
+long_string = "loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooongssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssstring"
+string_after_trimming = long_string[:97] + (long_string[97:] and '...')
+# result 'loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo...'
+
