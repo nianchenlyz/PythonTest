29b869c3f1fa118ec1a2caca0f5b395f23d04db4
Kamil Wozniak <kamil.m.wozniak@gmail.com>
2016-01-29 08:03:14 +0000

    Python requests ex - response

--- requests.py
+++ requests.py
@@ -4,4 +4,9 @@ domain = 'http://www.ford.co.uk/'
 
 def get_web_content(identifier):
     identifier = ''
-	url = '{}{}'.format(domain, identifier)
+    url = '{}{}'.format(domain, identifier)
+    
+    response = requests.get(url)
+    print response.status_code
+    print len(response.text) # or response.content
+
