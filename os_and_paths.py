434617d175e52168b9bc860d4336831ae7bcc271
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2016-02-05 21:43:03 +0000

    Absolute path and path join

---
+++ os_and_paths.py
@@ -0,0 +1,6 @@
+import os
+
+
+base_path = os.path.abspath('.')  # C:\\Users\\User_name or /home/user_name
+
+os.path.join('path', 'to', 'test', 'folder')  # path\\to\\test\\folder or path/to/test/folder
