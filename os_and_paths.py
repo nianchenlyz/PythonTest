e9bc1370cfe7cf206045404be79e2a05614eacd4
Kamil Woźniak <kamil.m.wozniak+git@gmail.com>
2016-02-23 23:15:17 +0000

    Example of eliminating duplicates

--- os_and_paths.py
+++ os_and_paths.py
@@ -1,4 +1,5 @@
 import os
+import re
 """ Useful information in documentation
     https://docs.python.org/2/library/os.path.html
 
@@ -18,3 +19,26 @@ os.listdir('.')
 
 for dirname, dirnames, filenames in os.walk('.'):
     print dirname, filenames
+
+
+# example of eliminating duplicates
+# file like `file0001_20160222.xml`
+previous_file_name = ''
+for dirname, dirnames, filenames in os.walk('.'):
+	filenames.sort()  # sorting files alphabetically
+	try:
+		for file_name in filenames:  # looping over all files in folder
+			filename_search = re.search('^(file\d{4})_\d{8}.xml$', file_name)  # regex
+			if filename_search and filename_search.group():  # when regex and group is not None
+				# will be True if file has same beginning, but different date
+				if filename_search.group(1) in previous_file_name:
+					# do something, remove, move file to different folder with shutil
+					continue  # temporary
+			previous_file_name = file_name
+	except Exception as e:
+		print 'Error occurred with exception: {}'.format(e.message)
+
+print 'All files checked'
+
+
+
