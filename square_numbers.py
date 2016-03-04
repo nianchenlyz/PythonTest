741248fe81f4ecadd6c445d9eea6bdea8b4e5d13
Kamil Wozniak <kamil.m.wozniak@gmail.com>
2016-03-04 20:08:28 +0000

    Square numbers example 1

---
+++ square_numbers.py
@@ -0,0 +1,9 @@
+def square_numbers1():
+    """ Computes square numbers
+
+    :return: list of square numbers
+    """
+    return [number*number for number in xrange(1,2001)]
+
+print square_numbers1()
+
